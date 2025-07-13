01a55132e0b8f0fec824cd3fceb71484bdf8250f
Akhtar Faizi Putra <akhtarerror@gmail.com>
2025-07-13 12:01:10 +0700

    first commit

---
+++ app/src/main/java/mber/suitmedia/myapplication/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package mber.suitmedia.myapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Orange gradient theme colors
+val Orange80 = Color(0xFFFFD54F)
+val OrangeGrey80 = Color(0xFFFFCC80)
+val OrangeAccent80 = Color(0xFFFFB74D)
+
+val Orange40 = Color(0xFFFF6B35)
+val OrangeGrey40 = Color(0xFFFF8E53)
+val OrangeAccent40 = Color(0xFFFFA726)
+
+// Additional orange shades
+val DarkOrange = Color(0xFFE65100)
+val LightOrange = Color(0xFFFFF3E0)
+val MediumOrange = Color(0xFFFF9800)
